4dc511a2b06f3d63b8107cbbce4dad01b92b24e1
youngs <lixiang@popicorns.com>
2024-10-02 14:45:25 +0800

    feat:手写main.ts 和 App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,16 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="app">hello world d</div>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+<script lang="ts">
+export default {
+  name: "App",
+};
+</script>
+<style>
+.app {
+  background: #ddd;
+  box-shadow: #ddd;
+  padding: 20px;
+  color: red;
 }
 </style>
