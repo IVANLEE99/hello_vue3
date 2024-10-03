f8181a017308b8112b86259e68e9d50647cc33df
youngs <lixiang@popicorns.com>
2024-10-03 20:48:35 +0800

    feat:ref 创建：基本类型的响应式数据

--- src/components/Person.vue
+++ src/components/Person.vue
@@ -13,21 +13,22 @@ export default {
   name: "Person",
 };
 </script> -->
-<script lang="ts" setup name='Person233'>
+<script lang="ts" setup name="Person233">
 console.log("setup"); //1、setup比 beforeCreate更先执行～
 console.log("@@this", this); //2、setup中的this为undefined，vue3中已经弱化this
 
+import { ref } from "vue";
 //3、数据，原来写在data中，此时name age tel都不是响应式的数据
-let name = "张三";
-let age = 18;
+let name = ref("张三");
+let age = ref(18);
 let tel = "1333";
 
 function changeName() {
-  name = "youngs001";
+  name.value = "youngs001";
   console.log(1, name); //4、注意这样修改name，页面是没有变化的
 }
 function changeAge() {
-  age += 1;
+  age.value += 1;
   console.log(2, age);
 }
 function showTel() {
