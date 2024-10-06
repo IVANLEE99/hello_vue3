3beb60069c75075f0a016b8c6e690a97a9299423
youngs <lixiang@popicorns.com>
2024-10-06 15:35:54 +0800

    feat:3.8. 【computed】

--- src/components/Person.vue
+++ src/components/Person.vue
@@ -1,30 +1,21 @@
 <!-- Person -->
 <template>
   <div class="person">
-    <h2>姓名：{{ person.name }}{{ name }}</h2>
-    <h2>年龄：{{ person.age }}{{ age }}{{ nl }}</h2>
-    <button @click="changeName">修改姓名</button>
-    <button @click="changeAge">修改年龄</button>
+    姓：<input type="text" v-model="firstName" />
+    <br />
+    名：<input type="text" v-model="lastName" />
+    <br />
+    全名：{{ fullName }}
   </div>
 </template>
 
 <script lang="ts" setup name="Person">
-import { reactive, toRefs, toRef } from "vue";
-let person = reactive({
-  name: "张三",
-  age: 18,
+import { ref, computed } from "vue";
+let firstName = ref("张");
+let lastName = ref("三");
+let fullName = computed(() => {
+  return firstName.value + "-" + lastName.value;
 });
-// let { name, age } = person;
-let { name, age } = toRefs(person);
-let nl = toRef(person, "age");
-function changeName() {
-  person.name += "~";
-  console.log(person.name, name);
-}
-function changeAge() {
-  person.age += 1;
-  console.log(person.age, age);
-}
 </script>
 <style scoped>
 .person {
