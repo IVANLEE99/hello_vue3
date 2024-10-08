4b9dea3ec10d54cb1d2f9b6a8b32207184577c07
youngs <lixiang@popicorns.com>
2024-10-08 15:23:54 +0800

    情况三：监视reactive定义的【对象类型】数据，且默认开启了深度监视。

--- src/components/Person.vue
+++ src/components/Person.vue
@@ -4,7 +4,7 @@
     <h2>watch 情况一 {{ sum }}</h2>
     <button @click="addSum">sum+1</button>
     <hr />
-    <h2>watch 情况二 监听ref定义的对象类型</h2>
+    <h2>watch 情况三 监听ref定义的对象类型</h2>
     name:{{ person.name }}
     <br />
     age:{{ person.age }}
@@ -16,7 +16,7 @@
 </template>
 
 <script lang="ts" setup name="Person">
-import { ref, computed, watch } from "vue";
+import { ref, computed, watch, reactive } from "vue";
 let sum = ref(0);
 function addSum() {
   sum.value += 1;
@@ -28,34 +28,30 @@ let stopWatch = watch(sum, (newval, oldval) => {
   }
 });
 
-let person = ref({
+let person = reactive({
   name: "张三",
   age: 9,
 });
 function changeName() {
-  person.value.name += "~";
+  person.name += "~";
 }
 function changeAge() {
-  person.value.age += 1;
+  person.age += 1;
 }
 function changePerson() {
-  person.value = {
+  Object.assign(person, {
     name: "李四",
     age: 44,
-  };
+  });
 }
 
-/*监视，情况一：监视【ref】定义的【对象类型】数据，监视的是对象的地址值，若想监视对象内部属性的变化，需要手动开启深度监视
-    watch的第一个参数是：被监视的数据
-    watch的第二个参数是：监视的回调
-    watch的第三个参数是：配置对象（deep、immediate等等.....） */
-let stopWatch2 = watch(
-  person,
-  (newVal, oldVal) => {
-    console.log("person 变了", newVal, oldVal);
-  },
-  { deep: false, immediate: false }
-);
+/*监视，
+监视reactive定义的【对象类型】数据，且默认开启了深度监视。
+
+ */
+let stopWatch2 = watch(person, (newVal, oldVal) => {
+  console.log("person 变了", newVal, oldVal);
+});
 </script>
 <style scoped>
 .person {
