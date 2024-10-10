8bf7ec9c9d93f645f26cbe2d7b040ea7a5e695ec
youngs <lixiang@popicorns.com>
2024-10-10 21:28:22 +0800

    watch

--- src/components/Person.vue
+++ src/components/Person.vue
@@ -4,14 +4,19 @@
     <h2>watch 情况一 {{ sum }}</h2>
     <button @click="addSum">sum+1</button>
     <hr />
-    <h2>watch 情况三 监听ref定义的对象类型</h2>
+    <h2>watch 情况四 监视ref或reactive定义的【对象类型】数据中的某个属性</h2>
     name:{{ person.name }}
     <br />
     age:{{ person.age }}
     <br />
+    汽车c1:{{ person.car.c1 }}
+    <br />
+    汽车c2:{{ person.car.c2 }}
     <button @click="changeName">修改名字</button>
     <button @click="changeAge">修改年龄</button>
-    <button @click="changePerson">修改人</button>
+    <button @click="changeC1">修改c1</button>
+    <button @click="changeC2">修改c2</button>
+    <button @click="changeCar">修改car</button>
   </div>
 </template>
 
@@ -31,6 +36,10 @@ let stopWatch = watch(sum, (newval, oldval) => {
 let person = reactive({
   name: "张三",
   age: 9,
+  car: {
+    c1: "奥迪",
+    c2: "宝马",
+  },
 });
 function changeName() {
   person.name += "~";
@@ -44,14 +53,52 @@ function changePerson() {
     age: 44,
   });
 }
+function changeC1() {
+  person.car.c1 = "雅迪";
+}
+function changeC2() {
+  person.car.c2 = "爱马";
+}
+function changeCar() {
+  person.car = {
+    c1: "车1",
+    c2: "车2",
+  };
+}
 
 /*监视，
-监视reactive定义的【对象类型】数据，且默认开启了深度监视。
-
+监视，情况四：监视响应式对象中的某个属性，且该属性是基本类型的，要写成函数式
  */
-let stopWatch2 = watch(person, (newVal, oldVal) => {
-  console.log("person 变了", newVal, oldVal);
-});
+watch(
+  () => person.name,
+  (newval, oldval) => {
+    console.log("person.name数据变化了");
+  }
+);
+// 监视，情况四：监视响应式对象中的某个属性，且该属性是对象类型的，可以直接写，也能写函数，更推荐写函数
+// watch(person.car, (newval, oldval) => {
+//   console.log("person.car数据变化了~", newval, oldval);
+// });
+watch(
+  () => person.car,
+  (newval, oldval) => {
+    // console.log("person.car数据变化了~", newval, oldval);
+  },
+  {
+    deep: true,
+  }
+);
+/*
+情况五
+监视上述的多个数据
+*/
+watch(
+  [() => person.name, () => person.car],
+  (newValue, oldValue) => {
+    console.log("person.car变化了", newValue, oldValue);
+  },
+  { deep: true }
+);
 </script>
 <style scoped>
 .person {
