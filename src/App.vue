f54c49681d1a0e3e46e23ac02fb12ffc8c8fd68c
youngs <lixiang@popicorns.com>
2024-10-25 15:29:27 +0800

    feat:ts type

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,6 @@
   <div class="app">
     <!-- vue3可以不需要跟节点 -->
     <Person ref="guangzhou"></Person>
-    <!-- <Person></Person> -->
-    <h5>guangzhou</h5>
-    <button @click="showLog">输出ref</button>
   </div>
 </template>
 <script lang="ts" setup name="App">
@@ -12,12 +9,6 @@ import Person from "./components/Person.vue";
 import { ref } from "vue";
 let guangzhou = ref();
 
-function showLog() {
-  console.log(guangzhou.value);
-  console.log(guangzhou.value.a);
-  console.log(guangzhou.value.b);
-  console.log(guangzhou.value.c);
-  console.log(guangzhou.value.guangzhou);
-}
+
 </script>
 <style></style>
--- src/components/Person.vue
+++ src/components/Person.vue
@@ -1,35 +1,32 @@
 <!-- Person -->
 <template>
   <div class="person">
-    <h1>广东</h1>
-    <h2 ref="guangzhou">广州</h2>
-    <h3>海珠</h3>
-    <button @click="showGuangzhou">获取广州</button>
+    {{ person.name }}
+    <br />
+    {{ persons }}
+    <br />
+    {{ ps }}
   </div>
 </template>
 
 <script lang="ts" setup name="Person">
-import { ref, defineExpose } from "vue";
-let guangzhou = ref();
-let a = ref(0);
-let b = ref(1);
-let c = ref(2);
-// console.log(guangzhou.value);
-function showGuangzhou() {
-  console.log(guangzhou.value);
-}
-defineExpose({
-  guangzhou,
-  a,
-  b,
-  c,
-});
+import { type PersonInter, type Persons } from "@/types";
+let person: PersonInter = { id: "1", name: "张三", age: 18 };
+let persons: Persons = [
+  { id: "1", name: "张三", age: 18 },
+  { id: "2", name: "李四", age: 19 },
+];
+let ps: Array<PersonInter> = [
+  { id: "1", name: "张三", age: 18 },
+  { id: "2", name: "李四", age: 19 },
+];
 </script>
 <style scoped>
 .person {
   background: skyblue;
   padding: 20px;
 }
+
 button {
   margin: 0 5px;
 }
