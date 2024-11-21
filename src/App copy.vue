3853b253f1fd4f1d5db377952d56f1186763c0cc
youngs <lixiang@popicorns.com>
2024-11-21 10:58:17 +0800

    feat:路由的基本切换效果

---
+++ src/App copy.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="app">
+    <!-- vue3可以不需要跟节点 -->
+    <Person></Person>
+  </div>
+</template>
+<script lang="ts" setup name="App">
+import Person from "./components/Person.vue";
+
+</script>
+<style></style>
---
+++ src/components/Home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="home">
+    <img
+      src="https://img5.su-cdn.com/cdn-cgi/image/format=webp/common/2024/11/18/1920x1080_c57da3b6d8a58f4628208f438505977a.jpg"
+      alt=""
+    />
+  </div>
+</template>
+
+<script setup lang="ts" name="Home"></script>
+
+<style scoped>
+.home {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+  img {
+    display: block;
+    margin: 0 auto;
+    height: 100%;
+  }
+}
+</style>
