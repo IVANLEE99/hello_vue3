b66f9a9ee8fc632b09c57a318ac94f12b88cde89
youngs <lixiang@popicorns.com>
2024-11-21 11:12:16 +0800

    feat:两个注意点

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -4,10 +4,17 @@
   </div>
 </template>
   
-  <script setup lang="ts" name="About">
+<script setup lang="ts" name="About">
+import { onMounted, onUnmounted } from "vue";
+onMounted(() => {
+  console.log("About组件挂载完毕");
+});
+onUnmounted(() => {
+  console.log("About组件卸载完毕");
+});
 </script>
-  
-  <style scoped>
+
+<style scoped>
 .about {
   display: flex;
   justify-content: center;
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
-import Home from "../components/Home.vue";
-import News from "../components/News.vue";
-import About from "../components/About.vue";
+import Home from "@/pages/Home.vue";
+import News from "@/pages/News.vue";
+import About from "@/pages/About.vue";
 
 const router = createRouter({
     history: createWebHistory(),
