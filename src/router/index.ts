bc0b8e13b02f997f6bfebd09e489b3f6649a30e1
youngs <lixiang@popicorns.com>
2024-11-21 11:25:49 +0800

    feat:路由_路由器工作模式

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,11 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHistory, createWebHashHistory } from "vue-router";
 import Home from "@/pages/Home.vue";
 import News from "@/pages/News.vue";
 import About from "@/pages/About.vue";
 
 const router = createRouter({
-    history: createWebHistory(),
+    // history: createWebHistory(),
+    history: createWebHashHistory(),
     routes: [
         {
             path: "/home",
