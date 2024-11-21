a0bc5cfed6b5e90f642ebf367c02ec92ffee0628
youngs <lixiang@popicorns.com>
2024-11-21 11:31:07 +0800

    feat:【to的两种写法】

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,9 @@
     <div class="navigate">
       <router-link to="/home" active-class="xiaozhupeiqi">首页</router-link>
       <router-link to="/news" active-class="xiaozhupeiqi">新闻</router-link>
-      <router-link to="/about" active-class="xiaozhupeiqi">关于</router-link>
+      <router-link :to="{ path: '/about' }" active-class="xiaozhupeiqi"
+        >关于</router-link
+      >
     </div>
     <div class="main-content">
       <router-view></router-view>
