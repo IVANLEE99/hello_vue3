54e099a21b81c8fea900ae77e0539720a577074e
youngs <lixiang@popicorns.com>
2024-11-26 12:01:29 +0800

    feat:修改数据(三种方式)

--- src/components/sum.vue
+++ src/components/sum.vue
@@ -18,6 +18,12 @@ const countStore = useCountStore();
 const n = ref(1);
 function add() {
   // sum.value += n.value;
+  // 第一种修改数据的方法
+  // countStore.count += n.value;
+  // 第二种修改数据的方法
+  countStore.$patch({ count: countStore.count + n.value, time: "dfdfd" });
+  // 第三种修改数据的方法
+  // countStore.increment(n.value);
 }
 function sub() {
   // sum.value -= n.value;
--- src/store/count.ts
+++ src/store/count.ts
@@ -1,5 +1,13 @@
 import { defineStore } from "pinia";
 
 export const useCountStore = defineStore("count", {
-    state: () => ({ count: 6 }),
+    actions: {
+        increment(n: number) {
+            this.count += n;
+        },
+    },
+    state: () => ({
+        count: 6,
+        time: "dfdf",
+    }),
 });
